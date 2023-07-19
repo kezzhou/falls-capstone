30d616974fa4be9221f04019042bc43ee7308a81
kezzhou <102555416+kezzhou@users.noreply.github.com>
2023-07-19 18:17:20 -0400

    Began laying foundation for mysqlworkbench and dropped unnecessary columns

--- descriptive.py
+++ descriptive.py
@@ -2,10 +2,68 @@
 import pandas as pd
 import numpy as np
 import datetime
+import mysql.connector as msql
+import sqlalchemy
 
 
-## Data Transformation
+## Data Cleaning and Transformation
 
+df = pd.read_csv("data/original/diagnosis.csv")
+df.drop(columns=['encounter_id', 'principal_diagnosis_indicator', 'admitting_diagnosis', 'reason_for_visit','derived_by_TriNetX', 'source_id'], inplace=True)
+df.to_csv('data/modified/diagnosis_mod.csv')
+
+df = pd.read_csv("data/original/encounter.csv")
+df.drop(columns=['start_date_derived_by_TriNetX', 'end_date_derived_by_TriNetX', 'derived_by_TriNetX'], inplace=True)
+df.to_csv('data/modified/encounter_mod.csv')
+
+df = pd.read_csv("data/original/lab_result.csv")
+df.drop(columns=['encounter_id', 'source_id', 'derived_by_TriNetX'], inplace=True)
+df.to_csv('data/modified/lab_result_mod.csv')
+
+df = pd.read_csv("data/original/medication_drug.csv")
+df.drop(columns=['encounter_id', 'unique_id', 'strength', 'quantity_dispensed','days_supply','derived_by_TriNetX', 'source_id'], inplace=True)
+df.to_csv('data/modified/medication_drug_mod.csv')
+
+df = pd.read_csv("data/original/medication_ingredient.csv")
+df.drop(columns=['encounter_id', 'unique_id', 'strength', 'brand','derived_by_TriNetX', 'source_id'], inplace=True)
+df.to_csv('data/modified/medication_ingredient_mod.csv')
+
+df = pd.read_csv("data/original/patient.csv")
+df.drop(columns=['reason_yob_missing', 'patient_regional_location', 'source_id'], inplace=True)
+df.to_csv('data/modified/patient_mod.csv')
+
+df = pd.read_csv("data/original/standardized_terminology.csv")
+df.drop(columns=['path'], inplace=True)
+df.to_csv('data/modified/standardized_terminology_mod.csv')
+
+df = pd.read_csv("data/original/vital_signs.csv")
+df.drop(columns=['derived_by_TriNetX', 'source_id'], inplace=True)
+df.to_csv('data/modified/vital_signs_mod.csv')
+
+
+
+## Pushing Data into MySqlWorkbench
+
+
+from mysql.connector import Error
+try:
+    conn = msql.connect(host='localhost', user='root',  
+                        password='')
+    if conn.is_connected():
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE AHI_falls")
+        print("Database is created")
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+# import the module
+from sqlalchemy import create_engine
+# create sqlalchemy engine
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"  
+                      .format(user="root", pw="", 
+                      db="AHI_falls"))
+# Insert whole DataFrame into MySQL
+irisData.to_sql('iris', con = engine, if_exists = 'append', chunksize = 1000,index=False)
 
 
 ## Demographic Data ##
