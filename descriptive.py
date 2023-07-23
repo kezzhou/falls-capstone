6b34572c243382bf8a08991daa8e8f16a9fbd8be
kezzhou <102555416+kezzhou@users.noreply.github.com>
2023-07-23 18:49:18 -0400

    Drop duplicates

--- descriptive.py
+++ descriptive.py
@@ -10,35 +10,59 @@ import sqlalchemy
 
 df = pd.read_csv("data/original/diagnosis.csv")
 df.drop(columns=['encounter_id', 'principal_diagnosis_indicator', 'admitting_diagnosis', 'reason_for_visit','derived_by_TriNetX', 'source_id'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/diagnosis_mod.csv')
+
 
 df = pd.read_csv("data/original/encounter.csv")
 df.drop(columns=['start_date_derived_by_TriNetX', 'end_date_derived_by_TriNetX', 'derived_by_TriNetX'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/encounter_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/lab_result.csv")
 df.drop(columns=['encounter_id', 'source_id', 'derived_by_TriNetX'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/lab_result_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/medication_drug.csv")
 df.drop(columns=['encounter_id', 'unique_id', 'strength', 'quantity_dispensed','days_supply','derived_by_TriNetX', 'source_id'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/medication_drug_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/medication_ingredient.csv")
 df.drop(columns=['encounter_id', 'unique_id', 'strength', 'brand','derived_by_TriNetX', 'source_id'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/medication_ingredient_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/patient.csv")
 df.drop(columns=['reason_yob_missing', 'patient_regional_location', 'source_id'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/patient_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/standardized_terminology.csv")
 df.drop(columns=['path'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/standardized_terminology_mod.csv')
+df.dtypes
 
 df = pd.read_csv("data/original/vital_signs.csv")
 df.drop(columns=['derived_by_TriNetX', 'source_id'], inplace=True)
+df.dtypes
+df.drop_duplicates()
 df.to_csv('data/modified/vital_signs_mod.csv')
+df.dtypes
 
 
 
